715ff99fdd8a3fb0fefe4240047ef52ec9b5b9d9
haruhisanagano-max <haruhisa_nagano@sincere-vision.com>
2026-05-22 10:22:51 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/aga'', // ← これを付け足してください！
+  basePath: '/aga', // ← これを付け足してください！
   typescript: {
     ignoreBuildErrors: true,
   },
