1fe47e09fe13ff0544fb8df891db7961f9c76e6a
haruhisanagano-max <haruhisa_nagano@sincere-vision.com>
2026-05-29 10:43:26 +0900

    Update pricing-plan.tsx

--- components/lp/pricing-plan.tsx
+++ components/lp/pricing-plan.tsx
@@ -131,6 +131,7 @@ export default function PricingPlan({ onOpenCampaign }: PricingPlanProps) {
           />
         </div>
 
+       
         
 
         {/* =========================================
