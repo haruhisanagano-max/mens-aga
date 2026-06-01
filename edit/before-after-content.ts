3b1d785572072e4d8554fc47f7328158a0b9de5c
haruhisanagano-max <haruhisa_nagano@sincere-vision.com>
2026-06-01 14:39:50 +0900

    Update before-after-content.ts

--- edit/before-after-content.ts
+++ edit/before-after-content.ts
@@ -3,7 +3,7 @@ export const BEFORE_AFTER_CONTENT = {
   sectionSubtitle: "科学的根拠に基づいた医療発毛が、あなたの未来を塗り替える。",
   
   gallery: [
-    { title: "", label: "", image: "https://lh3.googleusercontent.com/d/1GQ_KXoh_MpiL5HeJfmasfNHP13ie5W57" },
+    { title: "", label: "", image: "https://lh3.googleusercontent.com/d/16S5xwHvkit0vAw3mzD8dZXlKqulNZLWb" },
     
   ],
 
